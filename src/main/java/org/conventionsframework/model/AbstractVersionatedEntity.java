95e67769b150060b271bdff2a284ded58b661d6b
Everton Braga <evertonbragavieira@gmail.com>
2013-02-01 11:59:38 -0200

    Update src/main/java/org/conventionsframework/model/AbstractVersionatedEntity.java
    fixes #9

--- src/main/java/org/conventionsframework/model/AbstractVersionatedEntity.java
+++ src/main/java/org/conventionsframework/model/AbstractVersionatedEntity.java
@@ -40,6 +40,7 @@ public abstract class AbstractVersionatedEntity<T extends Serializable> extends 
     private Date createDate;
     private Date updateDate;
     private Integer version;
+    private String username;
     
       @Temporal(javax.persistence.TemporalType.TIMESTAMP)
     public Date getUpdateDate() {
@@ -66,6 +67,14 @@ public abstract class AbstractVersionatedEntity<T extends Serializable> extends 
 
     public void setVersion(Integer version) {
         this.version = version;
+    }
+    
+    public String getUsername() {
+        return username;    
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @PrePersist
