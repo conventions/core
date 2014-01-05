ee3adbfa971484c438552d8cc54de7ce20bec831
rmpestano <rmpestano@gmail.com>
2014-01-05 15:57:49 -0200

    removed mojarra dependency

--- src/main/java/org/conventionsframework/util/ResourceBundle.java
+++ src/main/java/org/conventionsframework/util/ResourceBundle.java
@@ -21,12 +21,13 @@
  */
 package org.conventionsframework.util;
 
-import com.sun.faces.renderkit.ApplicationObjectInputStream;
+import org.conventionsframework.qualifier.ConventionsBundle;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.text.MessageFormat;
-import org.conventionsframework.qualifier.ConventionsBundle;
 
 @ConventionsBundle
 public class ResourceBundle extends java.util.PropertyResourceBundle implements Serializable {
@@ -34,7 +35,7 @@ public class ResourceBundle extends java.util.PropertyResourceBundle implements 
     private static final long serialVersionUID = 1L;
 
     public ResourceBundle() throws IOException {
-        super(new ApplicationObjectInputStream());
+        super(new FileInputStream(""));
     }
 
     public ResourceBundle(InputStream stream) throws IOException {
